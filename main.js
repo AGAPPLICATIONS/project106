0dd1c9689c941312e83ea511a8a6a6fd8686ba8f
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2022-12-27 11:49:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
 //https://teachablemachine.withgoogle.com/models/8jWDM9tkW///
+function detectsounds() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/8jWDM9tkW/model.json", modelloaded);
+
+    function modelloaded() {
+        classifier.classify(gotResults);
+
+        function gotResults(error, result) {
+            if (result = error) {
+                console.log(error);
+
+            } else {
+                console.log(result);
+            }
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
